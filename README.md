26523348c2aa740a81e3a2ef80608eb548b22834
ztx6506 <2040858259@qq.com>
2023-11-08 23:19:01 +0800

    将ytbdl更改为yt-dlp

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # README
 ## 安装依赖
     pip install --upgrade -r requestments.text
-
+    apt install ffmpeg
+    sudo add-apt-repository ppa:tomtomtom/yt-dlp    
+    sudo apt update                                 
+    sudo apt install yt-dlp                         
--- download_video.py
+++ download_video.py
@@ -1,23 +1,23 @@
 import subprocess
-from socket import socket
-
-import socks
-SOCKS_PROXY_HOST = 'mg.ztx6506.link'
-SOCKS_PROXY_PORT = 20175
 
 
-def set_socks_proxy():
-    # 设置 SOCKS 代理
-    socks.set_default_proxy(socks.SOCKS5, SOCKS_PROXY_HOST, SOCKS_PROXY_PORT)
-    socket.socket = socks.socksocket  # 使用代理的套接字
+# from socket import socket
+#
+# import socks
+# SOCKS_PROXY_HOST = 'mg.ztx6506.link'
+# SOCKS_PROXY_PORT = 20175
+
+
+# def set_socks_proxy():
+#     # 设置 SOCKS 代理
+#     socks.set_default_proxy(socks.SOCKS5, SOCKS_PROXY_HOST, SOCKS_PROXY_PORT)
+#     socket.socket = socks.socksocket  # 使用代理的套接字
 
 
 def download(video_url):
-    download_dir = '/download'
-    download_directory = '/logs'
-    command = ["youtube-dl", "--verbose", "-o", f"{download_directory}/%(title)s.%(ext)s", video_url]
-    subprocess.call(command)
+    command = ['yt-dlp --proxy socks5://mg.ztx6506.link:20175',video_url]
+    subprocess.run(command)
 
 
 if __name__ == '__main__':
-    video_url = 'https://www.youtube.com/watch?v=e6MbVG8GO6s&lc=Ugydt8F8ECTvTxQEByx4AaABAg'
+    video_url = 'https://www.youtube.com/watch?v=VI-ACEwuFLQ'
