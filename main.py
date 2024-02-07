8b695c649dd67c02000cde338d0439ee86d02a22
ztx6506 <2040858259@qq.com>
2024-02-07 22:38:38 +0800

    错误弹窗

--- main.py
+++ main.py
@@ -1,88 +1,66 @@
-import os,time
-from tinydb import Query
-from googleapiclient.discovery import build
-import configparser
-from plugin.uploader import upload
-import plugin.translate as translate
-from plugin.webp_to_jpg import webp_to_jpg
-from plugin.db import DB
-import plugin.data as data
-def get_latest_video_link(api_key, channel_id):
-    youtube = build('youtube', 'v3', developerKey=api_key)
-    response = youtube.search().list(
-        channelId=channel_id,
-        maxResults=1,
-        order='date',
-        part='id'
-    ).execute()
-    video_id = response['items'][0]['id']['videoId']
-    video_response = youtube.videos().list(
-        id=video_id,
-        part='snippet'
-    ).execute()
-    video_info = {
-        'title': video_response['items'][0]['snippet']['title'],
-        'description': video_response['items'][0]['snippet']['description'],
-        'thumbnail': video_response['items'][0]['snippet']['thumbnails']['high']['url'],
-        'video_id': video_id
-    }
-    return video_info
-#下载视频
-def downloader(url):
-    os.system(f"yt-dlp --write-thumbnail -f'bestvideo+bestaudio' -o './output/{url}.%(ext)s' {url}")
-    webp_file_path = f'./output/{url}.webp'
-    jpg_output_path = f'./output/{url}.jpg'
-    webp_to_jpg(webp_file_path, jpg_output_path)
-    os.remove(webp_file_path)
-download_directory = 'output'
+
+# def mainloop():
+#     while True:
+#         for i in range(len(data.query_all())):
+#             channel_id = data.query_all()[i]['id']
+#             channel_name = data.query(i)
+#             config = configparser.ConfigParser()
+#             config.read("config.ini")
+#             url = get_latest_video_link('AIzaSyDE32cZMQK60oiniyXmI3CEMKMQvw2Hg7c', channel_id)
+#             print('视频标题:', url['title'])
+#             print('视频ID:', url['video_id'])
+#             trans = translate.trans(url['title'])
+#             print('翻译:', trans)
+#             my_db = DB()
+#             query = Query()
+#             if my_db.query(query.url == url['video_id']):
+#                 print('视频已存在')
+#             else:
+#                 my_db.add({'name': url['title'], 'url': url['video_id']})
+#                 downloader(url['video_id'])
+#                 print(url['video_id'])
+#                 print("下载完成")
+#                 print('开始上传')
+#                 video_file = f'./output/{url["video_id"]}.webm'
+#                 cover_file = f'./output/{url["video_id"]}.jpg'
+#                 desc = f'https://www.youtube.com/watch?v={url["video_id"]}'
+#                 title = "'" + "【" + channel_name + "】" + trans + "'"
+#                 tag = "'" + config.get(channel_name, 'tag') + "'"
+#                 uploader(cover_file, desc, tag, config.get(channel_name, 'tid'), title, video_file)
+#                 os.remove(video_file)
+#                 os.remove(cover_file)
+#                 print("上传完成")
+#                 my_db.close()
+#         for i in range(101):
+#             time.sleep(3)  # Simulate some work
+#             simple_progress_bar(i, 100, prefix='等待:', length=50)
+import plugins as pl
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html', index_Error =False)
+
+@app.route('/process',methods=['POST'])
+def process():
+    if request.method == 'POST':
+        if request.form.get('platform') == 'youtube':
+            try:
+                data = request.form['URL']
+                data=data.split('?v=')
+                print(data)
+                id = data[1]
+                print(id)
+            except IndexError :
+                return render_template('index.html',index_Error = True)
+        # pl.dv(data)
 
 
-def simple_progress_bar(current, total, prefix='', length=30, fill='😮‍💨', print_end='\r'):
-    percent = ("{0:.1f}").format(100 * (current / float(total)))
-    filled_length = int(length * current // total)
-    bar = fill * filled_length + '-' * (length - filled_length)
-    print(f'\r{prefix} |{bar}| {percent}% Complete', end=print_end)
+    return redirect(url_for('index'))
 
 
-def mainloop():
-    while True:
-        for i in range(len(data.query_all())):
-            channel_id=data.query_all()[i]['id']
-            channel_name=data.query(i)
-            config=configparser.ConfigParser()  
-            config.read("config.ini")
-            url = get_latest_video_link('AIzaSyDE32cZMQK60oiniyXmI3CEMKMQvw2Hg7c', channel_id)
-            print('视频标题:', url['title'])
-            print('视频ID:', url['video_id'])
-            trans=translate.trans(url['title'])
-            print('翻译:',trans)
-            my_db = DB()
-            query=Query()
-            if my_db.query(query.url == url['video_id']):
-                print('视频已存在')
-            else:
-                my_db.add({'name': url['title'], 'url': url['video_id']})
-                downloader(url['video_id'])
-                print(url['video_id'])
-                print("下载完成")
-                print('开始上传')
-                video_file=f'./output/{url["video_id"]}.webm'
-                cover_file=f'./output/{url["video_id"]}.jpg'
-                desc=f'https://www.youtube.com/watch?v={url["video_id"]}'
-                title="'"+"【"+channel_name+"】"+trans+"'"
-                tag="'"+config.get(channel_name,'tag')+"'"
-                upload(cover_file,desc, tag,config.get(channel_name,'tid'),title,video_file)
-                os.remove(video_file)
-                os.remove(cover_file)
-                print("上传完成")
-                my_db.close()
-        for i in range(101):
-                    time.sleep(3)  # Simulate some work
-                    simple_progress_bar(i, 100, prefix='等待:', length=50)
-        
-    
-    
-    
+
 
 if __name__ == '__main__':
-    mainloop()
+    app.run(debug=True)
---
+++ plugins.py
@@ -0,0 +1,250 @@
+import os, time, yt_dlp, platform
+from PIL import Image
+import urllib.parse
+import requests
+import json
+import re
+import ssl
+import ctypes
+
+
+# cover 封面 desc 简介 tag标签 tid分区 title标题
+def uploader_linux(cover, desc, tag, tid, title, file):
+    # os.system('./biliup login')
+    os.system(f'./biliup/biliup upload --cover {cover} --desc {desc} --tag {tag} --tid {tid} --title {title} {file}')
+    print(f'./biliup/biliup upload --cover {cover} --desc {desc} --tag {tag} --tid {tid} --title {title} {file}')
+def uploader_windows(cover, desc, tag, tid, title, file):
+    os.system(f'./biliup/biliup.exe upload --cover {cover} --desc {desc} --tag {tag} --tid {tid} --title {title} {file}')
+    print(f'./biliup/biliup.exe upload --cover {cover} --desc {desc} --tag {tag} --tid {tid} --title {title} {file}')
+
+# 下载视频
+
+
+def dv(url):
+    opt = {
+        'format': 'bestvideo+bestaudio',
+        'outtmpl': f'./video/{url}.%(ext)s',
+        'writethumbnail': True
+    }
+    with yt_dlp.YoutubeDL(opt) as dl:
+        dl.download(url)
+
+
+def webp_to_jpg(input_path, output_path):
+    try:
+        # 打开 WebP 图像
+        with Image.open(input_path) as img:
+            # 将 WebP 图像保存为 JPEG
+            img.convert("RGB").save(output_path, "JPEG")
+        print(f"Conversion successful: {output_path}")
+    except Exception as e:
+        print(f"Error converting WebP to JPEG: {e}")
+
+
+def simple_progress_bar(current, total, prefix='', length=30, fill='😮‍💨', print_end='\r'):
+    percent = ("{0:.1f}").format(100 * (current / float(total)))
+    filled_length = int(length * current // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    print(f'\r{prefix} |{bar}| {percent}% Complete', end=print_end)
+
+
+##翻译
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+class GoogleTrans(object):
+    def __init__(self):
+        self.url = 'https://translate.google.hk/translate_a/single'
+        self.TKK = "434674.96463358"  # 随时都有可能需要更新的TKK值
+
+        self.header = {
+            "accept": "*/*",
+            "accept-language": "zh-CN,zh;q=0.9",
+            "cookie": "NID=188=M1p_rBfweeI_Z02d1MOSQ5abYsPfZogDrFjKwIUbmAr584bc9GBZkfDwKQ80cQCQC34zwD4ZYHFMUf4F59aDQLSc79_LcmsAihnW0Rsb1MjlzLNElWihv-8KByeDBblR2V1kjTSC8KnVMe32PNSJBQbvBKvgl4CTfzvaIEgkqss",
+            "referer": "https://translate.google.hk/",  # cn域名已失效
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+            "x-client-data": "CJK2yQEIpLbJAQjEtskBCKmdygEIqKPKAQi5pcoBCLGnygEI4qjKAQjxqcoBCJetygEIza3KAQ==",
+        }
+
+        self.data = {
+            "client": "webapp",  # 基于网页访问服务器
+            "sl": "auto",  # 源语言,auto表示由谷歌自动识别
+            "tl": "vi",  # 翻译的目标语言
+            "hl": "zh-CN",  # 界面语言选中文，毕竟URL都是cn后缀了，就不装美国人了
+            "dt": ["at", "bd", "ex", "ld", "md", "qca", "rw", "rm", "ss", "t"],  # dt表示要求服务器返回的数据类型
+            "otf": "2",
+            "ssel": "0",
+            "tsel": "0",
+            "kc": "1",
+            "tk": "",  # 谷歌服务器会核对的token
+            "q": ""  # 待翻译的字符串
+        }
+
+    class JSHackToken():
+        def rshift(self, val, n):
+            return (val % 0x100000000) >> n
+
+        def Number(self, val):
+            try:
+                return eval(val, {}, {})
+            except:
+                return 0
+
+        class Undefined:
+            def __init__():
+                pass
+
+        class js_array():
+            def __init__(self, outer, init=[]):
+                self.outer = outer
+                self.storage = list(init).copy()
+
+            def __getitem__(self, key):
+                if (type(key).__name__ != 'int'):
+                    if (type(key).__name__ == 'float') and int(key) != key:
+                        return 0
+                    try:
+                        key = int(key)
+                    except:
+                        return 0
+                if len(self.storage) <= key or key < 0:
+                    return 0
+                return self.storage[key]
+
+            def __setitem__(self, key, value):
+                if (type(key).__name__ != 'int'):
+                    if (type(key).__name__ == 'float') and int(key) != key:
+                        return 0
+                    try:
+                        key = int(key)
+                    except:
+                        return 0
+                if key < 0:
+                    return 0
+                while key >= len(self.storage):
+                    self.storage.append(0)
+                self.storage[key] = value
+                return
+
+            def __len__(self):
+                return len(self.storage)
+
+            def __str__(self):
+                return self.storage.__str__()
+
+            def __repr__(self):
+                return self.storage.__repr__()
+
+        def array(self, init=[]):
+            return self.js_array(self, init)
+
+        def uo(self, a, b):
+            for c in range(0, len(b) - 2, 3):
+                d = b[c + 2]
+                if 'a' <= d:
+                    d = ord(d) - 87
+                else:
+                    d = self.Number(d)
+                if '+' == b[c + 1]:
+                    d = self.rshift(a, d)
+                else:
+                    d = a << d
+                if b[c] == "+":
+                    a = a + d & 4294967295
+                else:
+                    a = a ^ d
+            return a
+
+        def wo(self, a, tkk):
+            d = self.array(init=tkk.split("."))
+            b = self.Number(d[0])
+            e = self.array()
+            f = 0
+            g = 0
+            while g < len(a):
+                h = ord(a[g])
+                if 128 > h:
+                    e[f] = h
+                    f += 1
+                else:
+                    if 2048 > h:
+                        e[f] = h >> 6 | 192
+                        f += 1
+                    else:
+                        if (55296 == (h & 64512)) and (g + 1 < len(a)) and (56320 == (ord(a[g + 1]) & 64512)):
+                            h = 65536 + ((h & 1023) << 10) + (ord(a[g + 1]) & 1023)
+                            g += 1
+                            e[f] = h >> 18 | 240
+                            f += 1
+                            e[f] = h >> 12 & 63 | 128
+                            f += 1
+                        else:
+                            e[f] = h >> 12 | 224
+                            f += 1
+                            e[f] = h >> 6 & 63 | 128
+                            f += 1
+                    e[f] = h & 63 | 128
+                    f += 1
+                g += 1
+            a = b
+
+            for f in range(0, len(e)):
+                a += e[f]
+                a = ctypes.c_long(a).value
+                a = self.uo(a, '+-a^+6')
+            a = self.uo(a, '+-3^+b+-f')
+            a ^= self.Number(d[1])
+            if 0 > a:
+                a = (a & 2147483647) + 2147483648
+            a %= 10 ** 6
+            return str(a) + '.' + str(a ^ b)
+
+        # 构建完对象以后要同步更新一下TKK值
+        # self.update_TKK()
+
+    def update_TKK(self):
+        url = "https://translate.google.hk/"
+        req = requests.get(url, headers=self.header)
+        page_source = req.text
+        self.TKK = re.findall(r"tkk:'([0-9]+\.[0-9]+)'", page_source)[0]
+
+    def construct_url(self):
+        base = self.url + '?'
+        for key in self.data:
+            if isinstance(self.data[key], list):
+                base = base + "dt=" + "&dt=".join(self.data[key]) + "&"
+            else:
+                base = base + key + '=' + self.data[key] + '&'
+        base = base[:-1]
+        return base
+
+    def query(self, q, lang_to=''):
+        q = re.sub('''[^\u2E80-\u9FFF \n\t\w_.!'"“”`+-=——,$%^，。？、~@#￥%……|[\]&\\*《》<>「」{}【】()/]''', '', q)
+        retry = 3
+        while retry > 0:
+            try:
+                self.data['q'] = urllib.parse.quote(q)
+                self.data['tk'] = self.JSHackToken().wo(q, self.TKK)
+                self.data['tl'] = lang_to
+                url = self.construct_url()
+                robj = requests.post(url)
+                response = json.loads(robj.text)
+                targetText = ''
+                for item in response[0]:
+                    if item[0]:
+                        targetText += item[0]
+                originalText = response[0][0][1]
+                originalLanguageCode = response[2]
+                # print("翻译前：{}，翻译前code：{}".format(originalText, originalLanguageCode))
+                # print("=============================="
+                # print("翻译后：{}, 翻译后code：{}".format(targetText, lang_to))
+                # return originalText, originalLanguageCode, targetText, lang_to
+                return targetText
+            except Exception as e:
+                print(e)
+                retry -= 1
+                time.sleep(2)
+    # text = "Hello world"
+    # originalText, originalLanguageCode, targetText, targetLanguageCode = GoogleTrans().query(text, lang_to='zh-CN')
+    # print("==============================")
+    # print(originalText, originalLanguageCode, targetText, targetLanguageCode)
